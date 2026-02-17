3111e13df5d5400c6db4367e617cf28c3e35cdec
Haroon Riyaz <mirharoonriyaz@gmail.com>
2026-02-17 20:46:16 +0530

    Add static files to server -make basic ui for registration and login

---
+++ public/script.js
@@ -0,0 +1,36 @@
+const registerForm = document.getElementById("registerForm");
+const loginForm = document.getElementById("loginForm");
+
+registerForm.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+
+    const data = new FormData(registerForm);//collects all the input values from html form and pack them in special object
+    const body = Object.fromEntries(data.entries());// converts FormData into normal json like object
+
+    const res = await fetch("/api/register", {
+        method: "POST",
+        headers: {"Content-type": "application/json"},//the data is in json. 
+        body: JSON.stringify(body) //only JSON string travels over the network
+    })
+
+    const json = await res.json();
+    document.getElementById("registerMsg").innerText = json.message;  
+
+})
+
+loginForm.addEventListener("submit", async(e)=>{
+    e.preventDefault();
+
+    const data = new FormData(registerForm);
+    const body = Object.fromEntries(data.entries());
+
+    const res = await fetch("/api/login", {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(body)
+    })
+
+    const json = await res.json();
+    document.getElementById("loginMsg").innerText = json.message;
+    
+})
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config()
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
+app.use(express.static("public"));
 
 await connectDB();
 
